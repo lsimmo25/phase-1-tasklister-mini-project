1f1d88adf2865cbda18e842618fb2dd42bb6d10e
Lyle Simmons <lylesimmons125@gmail.com>
2024-02-03 12:49:36 -0500

    added due date field

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault()
-    buildToDo(e.target["new-task-description"].value)
+    const taskDescription = e.target["new-task-description"].value
+    const dueDate = e.target["due-date"].value
+    buildToDo(taskDescription, dueDate)
   })
 });
 
-function buildToDo(todo){
+function buildToDo(todo, dueDate){
   let li = document.createElement('li')
   let btn = document.createElement('button')
+  let p =document.createElement('p')
   btn.addEventListener('click', handleDelete)
   btn.textContent = 'x'
-  li.textContent = `${todo}  `
+  li.textContent = `${todo} Due: ${dueDate}  `
   li.appendChild(btn)
   document.querySelector('#tasks').appendChild(li)
 }
